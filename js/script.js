7a769986778c3aa0d7d963e9cd974eb6669a4e92
kaique456 <gemeoskaikau1221@gmail.com>
2024-07-02 16:51:02 -0300

    imagens agora passa para os lado

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var imageSlider = document.querySelector(".image-slider-container");
+
+function scrollLef() {
+    imageSlider.scrollLeft =- 200;
+}
+
+function scrollRight() {
+    imageSlider.scrollLeft += 200;
+}
+
+
